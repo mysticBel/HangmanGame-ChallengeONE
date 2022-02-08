6c562a255bf5711d3096f6bb0e545cc0d8b1f404
mysticBel <dnxbel@gmail.com>
2022-02-08 13:00:54 -0500

    adding style and background, footer done

--- js/main.js
+++ js/main.js
@@ -1,4 +1,36 @@
+let guessedWordCategory, guessedWordRandomPicked; 
+let listRandomWords = [];
+let attempts = 0;
 
+const ListWords = [
+    word1 = {
+        nome: "cookie",
+        categoria: "sweet - food"
+    },
+    word2 = {
+        nome: "marshmellow",
+        categoria: "sweet - food"
+    },
+    word3 = {
+        nome: "cake",
+        categoria: "sweet - food"
+    }
+]
+
+function isChosenKey(key) {
+    document.getElementById("key-" + key).disabled = true;
+    if (attempts > 0) {
+        changeKeyStyle("key-" + key);
+        compareKeys(key);
+        printKey();
+    }
+
+}
+
+function  changeKeyStyle(key) {
+    document.getElementById(key).style.background = "#C71585";
+    document.getElementById(key).style.color = "#ffffff";
+}
 
 
 // Reload
